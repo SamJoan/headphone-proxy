4cb7b44fb320938fd90aba6bbc6c6bbbe22f284d
Pedro Worcel <pedro@worcel.com>
2017-04-16 13:29:14 +1200

    Tidy controller code, fix table.

--- mallory/ui.py
+++ mallory/ui.py
@@ -1,19 +1,43 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QTableView, QAction, QApplication
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QAbstractTableModel
+from PyQt5.QtCore import QAbstractTableModel, Qt, QVariant
 
 class RequestHistoryModel(QAbstractTableModel):
-    def rowCount(self, _):
-        return 1
+    def __init__(self, datain, headerdata, parent=None, *args):
+        """ datain: a list of lists
+            headerdata: a list of strings
+        """
+        QAbstractTableModel.__init__(self, parent, *args)
+        self.arraydata = datain
+        self.headerdata = headerdata
 
-    def columnCount(self, _):
-        return 5
+    def rowCount(self, parent):
+        return len(self.arraydata)
 
-    def data(self, _, a):
-        return [
-            ("http:/google.com", "GET", "/admin", "a", "aa")
-        ]
+    def columnCount(self, parent):
+        return len(self.arraydata[0])
+
+    def data(self, index, role):
+        if not index.isValid():
+            return QVariant()
+        elif role != Qt.DisplayRole:
+            return QVariant()
+        return QVariant(self.arraydata[index.row()][index.column()])
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return QVariant(self.headerdata[col])
+        return QVariant()
+
+    def sort(self, Ncol, order):
+        """Sort table by given column number.
+        """
+        self.emit(SIGNAL("layoutAboutToBeChanged()"))
+        self.arraydata = sorted(self.arraydata, key=operator.itemgetter(Ncol))
+        if order == Qt.DescendingOrder:
+            self.arraydata.reverse()
+        self.emit(SIGNAL("layoutChanged()"))
 
 class HPMainWindow(QMainWindow):
     def __init__(self):
@@ -21,32 +45,38 @@ class HPMainWindow(QMainWindow):
 
         self.initUI()
 
-    def initUI(self):
-
-        tableView = QTableView()
-        tableView.setModel(RequestHistoryModel())
-        self.setCentralWidget(tableView)
-
+    def _exitAction(self):
         smileyIcon = QIcon('icons/smiley.png')
         exitAction = QAction(smileyIcon, 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Smiley face will wreck your shit.')
+
         exitAction.triggered.connect(self.close)
 
+        return exitAction
+
+    def initUI(self):
+        m = RequestHistoryModel([["a", "b", "c"]], ["colA","colB","colC"])
+        tableView = QTableView()
+        tableView.setModel(m)
+
         self.statusBar()
+        self.setCentralWidget(tableView)
+        self.setGeometry(300, 300, 1024, 768)
+        self.setWindowTitle('Headphone HTTP Proxy')
+        self.setWindowIcon(QIcon('icons/smiley.png'))
+        self.initButtons()
+        self.show()
+
+    def initButtons(self):
+        exitAction = self._exitAction()
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAction)
 
-        toolbar = self.addToolBar('Exit')
+        toolbar = self.addToolBar('MainToolbar')
         toolbar.addAction(exitAction)
-
-        self.setGeometry(300, 300, 800, 600)
-        self.setWindowTitle('Headphone HTTP Proxy')
-        self.show()
-
-        self.setWindowIcon(smileyIcon)
 
 def init():
     app = QApplication(sys.argv)
