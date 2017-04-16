912fbfb5b3d179f4ec085f0f646ace6983fc86c0
Pedro Worcel <pedro@worcel.com>
2017-04-16 15:02:13 +1200

    Update layout.

--- mallory/ui.py
+++ mallory/ui.py
@@ -1,37 +1,42 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QTableView, QAction, QApplication
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QAbstractTableModel, Qt, QVariant
 
 class RequestHistoryModel(QAbstractTableModel):
-    def __init__(self, datain, headerdata, parent=None, *args):
-        """ datain: a list of lists
-            headerdata: a list of strings
+    def __init__(self, data, parent=None, *args):
+        """
+        data should contain a list of dicts for now
         """
         QAbstractTableModel.__init__(self, parent, *args)
-        self.arraydata = datain
-        self.headerdata = headerdata
+        self.arraydata = data
 
     def rowCount(self, parent):
         return len(self.arraydata)
 
     def columnCount(self, parent):
-        return len(self.arraydata[0])
+        return len(self.arraydata[0].keys())
 
     def data(self, index, role):
         if not index.isValid():
             return QVariant()
         elif role != Qt.DisplayRole:
             return QVariant()
-        return QVariant(self.arraydata[index.row()][index.column()])
+
+        col = index.column()
+        key = list(self.arraydata[index.row()].keys())[index.column()]
+
+        return QVariant(self.arraydata[index.row()][key])
 
     def headerData(self, col, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return QVariant(self.headerdata[col])
+            return QVariant(list(self.arraydata[0].keys())[col])
+
         return QVariant()
 
     def sort(self, Ncol, order):
-        """Sort table by given column number.
+        """
+        Sort table by given column number.
         """
         self.emit(SIGNAL("layoutAboutToBeChanged()"))
         self.arraydata = sorted(self.arraydata, key=operator.itemgetter(Ncol))
@@ -55,13 +60,37 @@ class HPMainWindow(QMainWindow):
 
         return exitAction
 
+    def _initFakeModel(self):
+        fakeRequest = {
+            "id": "1",
+            "host": "http://localhost",
+            "method": "GET",
+            "path": "/test/index.php",
+            "response_status": "200",
+            "length": "300",
+            "title": "HOw to hax0r.",
+        }
+
+        fakeData = []
+        for nb in range(35):
+            fakeData.append(fakeRequest)
+
+        return fakeData
+
     def initUI(self):
-        m = RequestHistoryModel([["a", "b", "c"]], ["colA","colB","colC"])
+        centralWidget = QWidget()
+        layout = QVBoxLayout()
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+
+        m = RequestHistoryModel(self._initFakeModel())
         tableView = QTableView()
+        tableView.setSelectionBehavior(QTableView.SelectRows)
         tableView.setModel(m)
+        layout.addWidget(tableView)
+        layout.addWidget(QTextEdit())
 
         self.statusBar()
-        self.setCentralWidget(tableView)
         self.setGeometry(300, 300, 1024, 768)
         self.setWindowTitle('Headphone HTTP Proxy')
         self.setWindowIcon(QIcon('icons/smiley.png'))
